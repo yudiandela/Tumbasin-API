92c6a45d2b2ced5b96fc55013e84c217dd7eb821
Yudi Andela <yudhi.andhela@gmail.com>
2019-10-16 22:25:42 +0700

    Merapikan template navigasi

--- resources/views/layouts/_partials/sidenav.blade.php
+++ resources/views/layouts/_partials/sidenav.blade.php
@@ -1,18 +1,23 @@
 <nav class="nav flex-column">
     <a class="nav-link border-right"></a>
-    <a class="nav-link text-muted border border-right-0" href="{{ route('dashboard.index') }}">
+    <a class="nav-link {{ strpos(Route::currentRouteName(), 'dashboard') !== false ? 'border border-right-0 text-muted' : 'border-right'}}"
+        href="{{ route('dashboard.index') }}">
         <i class="mr-1 fas fa-tachometer-alt"></i> Dashboard
     </a>
-    <a class="nav-link border-right" href="{{ route('brand.index') }}">
+    <a class="nav-link {{ strpos(Route::currentRouteName(), 'brand') !== false ? 'border border-right-0 text-muted' : 'border-right'}}" 
+        href="{{ route('brand.index') }}">
         <i class="mr-1 fas fa-tags"></i> Brands
     </a>
-    <a class="nav-link border-right" href="{{ route('category.index') }}">
+    <a class="nav-link {{ strpos(Route::currentRouteName(), 'category') !== false ? 'border border-right-0 text-muted' : 'border-right'}}" 
+        href="{{ route('category.index') }}">
         <i class="mr-1 fas fa-archive"></i> Categories
     </a>
-    <a class="nav-link border-right" href="{{ route('product.index') }}">
+    <a class="nav-link {{ strpos(Route::currentRouteName(), 'product') !== false ? 'border border-right-0 text-muted' : 'border-right'}}" 
+        href="{{ route('product.index') }}">
         <i class="mr-1 fas fa-box-open"></i> Products
     </a>
-    <a class="nav-link border-right" href="{{ route('order.index') }}">
+    <a class="nav-link {{ strpos(Route::currentRouteName(), 'order') !== false ? 'border border-right-0 text-muted' : 'border-right'}}" 
+        href="{{ route('order.index') }}">
         <i class="mr-1 fas fa-shopping-cart"></i> Order
     </a>
     <a class="nav-link border-right"></a>
